0a7c45aa1527a62db46286ed7478aec7fe5d54d8
Imre Mihaly <imre.mihaly@gmail.com>
2022-02-21 19:59:00 +0100

    mostly comments

--- lib/BB.hpp
+++ lib/BB.hpp
@@ -23,17 +23,45 @@ namespace chessbox {
         static constexpr BitBoard allSquares = -1;
         
     public:
+        
+        // returns the bitboard representation of the given square.
         static const BitBoard bitboard(const Square square);
+        
+        // returns the bitboard representation of the given rank.
         static const BitBoard rank(int rankIdx);
+        
+        // returns the bitboard representation of the given file.
         static const BitBoard file(int fileIdx);
+        
+        // returns the bitboard representation of the square adjacent to the given `square` in the given `direction`.
         static const BitBoard adjacent(const Square square, Direction dir);
+        
+        // returns the bitboard representation of the squares adjacent to the given `square`.
         static const BitBoard adjacents(const Square square);
+        
+        // returns the bitboard representation of a partial -file, -rank or -diagonal starting near the `square` and running in the given `direction`.
+        // example:
+        //   if square == e4 and direction is `North` the returned bitboard contains the squares: { e5, e6, e7, e8 }
         static const BitBoard line(const Square square, Direction dir);
+        
+        // returns the bitboard representation containing all lines starting at `square` and running in any direction.
+        // see: `BitBoard line(const Square square, Direction dir)`
         static const BitBoard lines(const Square square);
+        
+        // returns the direction in which the square `to` id from square `from`.
+        // returns `Direction::Any` if to == from
+        // returns `Direction::Unknown` if to and from is not on the same file, rank or diagonal.
         static const Direction dir(const Square from, const Square to);
+        
+        // returns the collection of squares between `from` and `too` if they are on teh same line (file, rank or diagonal).
+        // returns Bitboard(0) otherwise.
+        // `to` and `from` are not included into the collection.
         static const BitBoard range(const Square from, const Square to);
         
+        // returns the collection of rookLines (file or rank) containing the given `square`.
         static const BitBoard rookLines(const Square square);
+        
+        
         static const BitBoard bishopLines(const Square square);
         static const BitBoard knightDistances(const Square square);
         static const BitBoard pawnCaptures(const Square square, const Color color);
